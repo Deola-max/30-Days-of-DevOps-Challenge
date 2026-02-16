ac749baae97e70ed4d5744600d5832b7fe848f09
Deola-max <favouradeola315@gmail.com>
2026-02-16 12:37:25 +0100

    Day 4: System Resource Monitoring Script

---
+++ day04/resource_monitor.sh
@@ -0,0 +1,11 @@
+# 1. Go to your main project folder
+cd ~/30-days-devops
+
+# 2. Tell Git to prepare the Day 4 folder
+git add day04/
+
+# 3. Write your "stamp" for the day
+git commit -m "Day 4: System Resource Monitoring Script"
+
+# 4. Push it to the cloud
+git push origin main
